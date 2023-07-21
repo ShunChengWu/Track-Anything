fdfd55c582ff4839faeebe6453f3b832b7501623
Shun-Cheng <shuncheng.wu91@gmail.com>
2023-07-21 17:00:44 +0200

    add colmap tracker gui

--- colmap_tracker.py
+++ colmap_tracker.py
@@ -15,21 +15,28 @@ from tools.interact_tools import SamControler
 
 
 class Image():
-    def __init__(self,idx:int, image:np.array):
+    def __init__(self,idx:int, image:np.array, name:str):
         self.idx = idx
         self.image = image
+        self.name = name
+    def __lt__(self,other):
+        return self.idx < other.idx
 
 class ColmapTrackerSegmentAnything():
-    def __init__(self, sam_checkpoint, path_colmap, args):
+    def __init__(self, sam_checkpoint, args):
+        self.colmap_inited = False
         '''load colmap'''
-        self.reset_colmap(path_colmap)
         self.reset_sam(sam_checkpoint,args)
+        
+    def init_colmap(self,path):
+        self.reset_colmap(path)
         
     def reset_colmap(self, path:str):
         _ ,self.imgs, pts = read_model(path)
         self.pt_scores = {k:0 for k in pts}
         # get incremental ids
-        self.img_ids = sorted([img.id for img in self.imgs.values()])
+        self.imgs_info = sorted([Image(idx=img.id,image=None,name=img.name) for img in self.imgs.values()])
+        self.colmap_inited = True
         
     def reset_sam(self, path:str, args):
         self.samcontroler = SamControler(path, args.sam_model_type, args.device)
@@ -41,7 +48,7 @@ class ColmapTrackerSegmentAnything():
         mask, logit, painted_image = self.samcontroler.first_frame_click(image.image, points, labels, multimask)
         tracked_2D_pts, tracked_3D_pts = self.get_tracked_pts(image.idx)
         self.update_score(mask, tracked_2D_pts, tracked_3D_pts)
-        return mask, logit, Image(image.idx,painted_image)
+        return mask, logit, Image(image.idx,painted_image,image.name)
     
     def generator(self, images: list, multimask:bool=True):
         masks = []
@@ -89,7 +96,7 @@ class ColmapTrackerSegmentAnything():
                     break
                 
             self.samcontroler.sam_controler.reset_image()
-            self.samcontroler.sam_controler.set_image(frame)
+            self.samcontroler.sam_controler.set_image(image.image)
             mask, logit, painted_image = self.samcontroler.first_frame_click(
                 image.image, 
                 np.array(points), 
@@ -111,7 +118,7 @@ class ColmapTrackerSegmentAnything():
             
             masks.append(mask)
             logits.append(logit)
-            painted_images.append(Image(image.idx,painted_image))
+            painted_images.append(Image(image.idx,painted_image,name=image.name))
             
         return masks, logits, painted_images
     
@@ -160,6 +167,7 @@ def download_checkpoint(url, folder, filename):
 def parse_augment():
     parser = argparse.ArgumentParser()
     parser.add_argument('--exp_name', type=str,default='test_colmap_track_2')
+    parser.add_argument('--colmap_model', type=str, default="", required=True)
     parser.add_argument('--device', type=str, default="cuda:0")
     parser.add_argument('--sam_model_type', type=str, default="vit_h")
     parser.add_argument('--port', type=int, default=6080, help="only useful when running gradio applications")  
@@ -184,9 +192,12 @@ if __name__ == '__main__':
     4. Generate clicks
     
     """
+    
     path_colmap = '/home/sc/data/HLoc_s0.5fps2/colmap_text'
     path_images = '/home/sc/data/s0.5fps2/images_resized'
     args = parse_augment()
+    
+    path_colmap = args.colmap_model
     
     path_out = os.path.join('result',args.exp_name)
     pathlib.Path(path_out).mkdir(exist_ok=True,parents=True)
@@ -195,7 +206,7 @@ if __name__ == '__main__':
     # cams ,imgs, pts = read_model(path_colmap)
     # pt_scores = {k:0 for k in pts}
     # # get incremental ids
-    # img_ids = sorted([img.id for img in imgs.values()])
+    # imgs_info = sorted([img.id for img in imgs.values()])
     
     # Show image and their keypoints
     # img = drawKeyPoints(imgs[1],path_images)
@@ -224,13 +235,13 @@ if __name__ == '__main__':
 
 
     images = []
-    for img_id in tqdm(ctsam.img_ids,desc="Load images to buffer"):
-        img = ctsam.imgs[img_id]
+    for img_info in tqdm(ctsam.imgs_info,desc="Load images to buffer"):
+        img = ctsam.imgs[img_info.idx]
         # Load image
         image_name = os.path.join(path_images,"{0:04d}.png".format(img.id))
         frame = cv2.imread(image_name, cv2.IMREAD_COLOR)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        images.append(Image(img_id,frame))
+        images.append(Image(img_info.idx,frame))
         
         if len(images)>2:break
         
@@ -258,7 +269,7 @@ if __name__ == '__main__':
     painted_image.image.save(os.path.join(path_out_img,"tmp_painted_img_{0:04d}.png".format(painted_image.idx)))
         
     masks, logits, painted_images = ctsam.generator(
-        images,
+        images[1:],
     )
     
     # Save
@@ -271,113 +282,3 @@ if __name__ == '__main__':
         pathlib.Path(path_out_mask_ngp).mkdir(exist_ok=True,parents=True)
         name = os.path.join(path_out_mask_ngp,'dynamic_mask_{0:04d}.png'.format(img.id))
         cv2.imwrite(name,1-mask)
-
-        
-    # ### Main Loop
-    # pbar = tqdm(img_ids)
-    # for img_id in pbar:
-    #     img = imgs[img_id]
-    #     # Get tracked keypoints
-    #     filter_idx = np.where(img.point3D_ids>=0)
-    #     filtered_img = img.xys[filter_idx].round().astype(int)
-    #     filtered_pts = img.point3D_ids[filter_idx]
-        
-    #     # Load image
-    #     image_name = os.path.join(path_images,"{0:04d}.png".format(img.id))
-    #     frame = cv2.imread(image_name, cv2.IMREAD_COLOR)
-    #     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    #     # reset
-    #     # samcontroler.sam_controler.reset_image()
-    #     # samcontroler.sam_controler.set_image(frame)
-        
-    #     # Generate promts
-    #     if img_id == img_ids[0]: 
-    #         # First click
-    #         points = [
-    #             # [int(frame.shape[0]/2), int(frame.shape[1]/2)],
-    #             [ 100, 500],
-                
-    #             [ 100, 250]
-    #         ] # [x,y]
-    #         labels = [
-    #             1, 
-    #             0
-    #         ] # 1: postiive. 0: negative
-            
-    #         # Predict
-    #         ctsam.first_frame_click(
-    #             img_id,
-    #             frame,
-    #             np.array(points), 
-    #             np.array(labels), 
-    #             multimask
-    #         )
-            
-    #         # samcontroler.sam_controler.reset_image()
-    #         # samcontroler.sam_controler.set_image(frame)
-    #         # mask, logit, painted_image = samcontroler.first_frame_click(
-    #         #     frame, 
-    #         #     np.array(points), 
-    #         #     np.array(labels), multimask)
-    #     else:
-            
-    #         non_zero_scores = np.array([score for score in pt_scores.values() if score != 0])
-    #         score_std = np.std(np.abs(non_zero_scores))
-    #         threshold = np.abs(non_zero_scores).mean()
-        
-    #         print('threshold: {} std: {}'.format(threshold,score_std))
-    #         while True:
-    #             points = []
-    #             labels = []
-                
-    #             for pt_id, img_xy in zip(filtered_pts,filtered_img):
-    #                 score = pt_scores[pt_id]
-                    
-    #                 #TODO: maybe only filter positive?
-    #                 if score >=0:
-    #                     if abs(score) < threshold: continue 
-                    
-    #                 label = 1 if score > 0 else -1
-    #                 point = [img_xy[0],img_xy[1]]
-                    
-    #                 points.append(point)
-    #                 labels.append(label)
-    #             if len(points) == 0:
-    #                 # reduce threshold
-    #                 threshold -= score_std
-    #                 print('reduce threshold to {}'.format(threshold))
-    #             else:
-    #                 break
-        
-    #         mask, logit, painted_image = samcontroler.first_frame_click(
-    #             frame, 
-    #             np.array(points), 
-    #             np.array(labels), 
-    #             multimask)
-            
-    #         # TRY mask input
-    #         prompts = {
-    #             'point_coords': np.array(points),
-    #             'point_labels': np.array(labels),
-    #             'mask_input': logit[None,:,:]
-    #         }
-    #         mask, logit, painted_image = samcontroler.predict(
-    #             frame,
-    #             prompts,
-    #             multimask=True)
-        
-        
-    #     # Update score
-    #     mask_score = mask[filtered_img[:,1],filtered_img[:,0]] #
-    #     for pt_id, score in zip(filtered_pts, mask_score):
-    #         pt_scores[pt_id] += 1 if score == True else -1
-        
-    #     # Save
-    #     path_out_img = os.path.join(path_out,"image")
-    #     pathlib.Path(path_out_img).mkdir(exist_ok=True,parents=True)
-    #     painted_image.save(os.path.join(path_out_img,"tmp_painted_img_{0:04d}.png".format(img.id)))
-        
-    #     path_out_mask_ngp = os.path.join(path_out,"mask_instant-ngp")
-    #     pathlib.Path(path_out_mask_ngp).mkdir(exist_ok=True,parents=True)
-    #     name = os.path.join(path_out_mask_ngp,'dynamic_mask_{0:04d}.png'.format(img.id))
-    #     cv2.imwrite(name,1-mask)
